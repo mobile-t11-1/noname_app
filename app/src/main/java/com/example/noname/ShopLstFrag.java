a9f2e91ca2a335098969b271d362b74e25702ebd
HaoRun Yang <haoruny@student.unimelb.edu.au>
2021-09-24 08:51:37 +0800

    Initialize shopping list feature
    Initialize the list architecture, the list item , try to connect to firebase (no testing)

--- app/src/main/java/com/example/noname/ShopLstFrag.java
+++ app/src/main/java/com/example/noname/ShopLstFrag.java
@@ -2,11 +2,22 @@ package com.example.noname;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,14 +26,12 @@ import android.view.ViewGroup;
  */
 public class ShopLstFrag extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ListView list;
+    private ImageView checkbox, atTop;
+    private TextView listTitle, dueDate;
 
     public ShopLstFrag() {
         // Required empty public constructor
@@ -32,16 +41,12 @@ public class ShopLstFrag extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment ShoppingList.
      */
     // TODO: Rename and change types and number of parameters
-    public static ShopLstFrag newInstance(String param1, String param2) {
+    public static ShopLstFrag newInstance() {
         ShopLstFrag fragment = new ShopLstFrag();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,10 +54,34 @@ public class ShopLstFrag extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        list = getView().findViewById(R.id.listview);
+        checkbox = getView().findViewById(R.id.checkbox);
+        atTop = getView().findViewById(R.id.atTop);
+        listTitle = getView().findViewById(R.id.listTitle);
+        dueDate = getView().findViewById(R.id.dueDate);
+
+
+        db.collection("list")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                document.getId();
+                                document.getData();
+                            }
+                        } else {
+
+                        }
+                    }
+                });
+
     }
 
     @Override
