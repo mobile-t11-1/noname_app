9eecc3065ee8033e460c22b01bad81f7dd328977
HaoRun Yang <haoruny@student.unimelb.edu.au>
2021-10-03 23:40:23 +0800

    UI changed, dynamic loading data
    1. Change UI according to new design.
    2. Connect to the database, dynamically add list items to the list
    3. Defect: timestamp -> string, the text size of due date should be smaller, title and due date should have longer display space.
    4. TODO:
    1) New activity for displaying details of each item
    2) New list and corresponding component

--- app/src/main/java/com/example/noname/ShopLstFrag.java
+++ app/src/main/java/com/example/noname/ShopLstFrag.java
@@ -5,19 +5,32 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.AbsoluteSizeSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +42,7 @@ public class ShopLstFrag extends Fragment {
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
 
+    private View view;
     private ListView list;
     private ImageView checkbox, atTop;
     private TextView listTitle, dueDate;
@@ -54,40 +68,57 @@ public class ShopLstFrag extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        mAuth = FirebaseAuth.getInstance();
-        db = FirebaseFirestore.getInstance();
-
-        list = getView().findViewById(R.id.listview);
-        checkbox = getView().findViewById(R.id.checkbox);
-        atTop = getView().findViewById(R.id.atTop);
-        listTitle = getView().findViewById(R.id.listTitle);
-        dueDate = getView().findViewById(R.id.dueDate);
-
-
-        db.collection("list")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-
-                                document.getId();
-                                document.getData();
-                            }
-                        } else {
-
-                        }
-                    }
-                });
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shopping_list, container, false);
+        view = inflater.inflate(R.layout.fragment_shopping_list, container, false);
+        list = view.findViewById(R.id.list);
+
+
+        db.collection("list")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        List<Map<String,Object>> allitems = new ArrayList<>();
+
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Map<String,Object> map = new HashMap<>();
+                            Map<String, Object> data = document.getData();
+                            DateFormat sdf = new SimpleDateFormat("MM dd, yyyy");
+
+                            if((boolean) data.get("top")) {
+                                map.put("favorite", R.drawable.ic_list_heart_full);
+                            } else {
+                                map.put("favorite", R.drawable.ic_list_heart_empty);
+                            }
+
+                            String title = data.get("title").toString();
+                            String dueDate = data.get("dueDate").toString();
+                            SpannableString formatString = new SpannableString(title + "\n"
+                                    + dueDate);
+                            formatString.setSpan(new AbsoluteSizeSpan(20),
+                                    0, title.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            formatString.setSpan(new AbsoluteSizeSpan(15),
+                                    title.length()+1, title.length()+dueDate.length()+1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                            map.put("title", formatString);
+                            allitems.add(map);
+                        }
+                        SimpleAdapter items = new SimpleAdapter(getActivity().getApplicationContext(), allitems, R.layout.fragment_shopping_list_item,
+                                new String[]{"favorite", "title"},
+                                new int[]{R.id.favorite, R.id.listTitle});
+                        list.setAdapter(items);
+                    } else {
+
+                    }
+                });
+
+        return view;
     }
 }
