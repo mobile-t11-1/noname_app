386b6cc924990c7550ef3fb44358dbc523864d75
Zhenyang Huang <u6338988@anu.edu.au>
2021-10-27 02:30:35 +1100

    enable initial upload-avatar function through taking photos by camera

--- app/src/main/java/com/example/noname/EditFrag.java
+++ app/src/main/java/com/example/noname/EditFrag.java
@@ -1,14 +1,21 @@
 package com.example.noname;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -33,6 +40,11 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -54,14 +66,11 @@ public class EditFrag extends Fragment {
 
     private FirebaseAuth fAuth;
     private StorageReference storage;
-    private FirebaseFirestore db =  FirebaseFirestore.getInstance();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     private EditText username, oldpassword, newpassword, confirmpassword;
-    private Button avatar_btn, submit_btn, back_btn;
+    private Button avatar_btn, submit_btn, back_btn, camera_btn;
     private ImageView avatar;
-
-
-
 
 
     public EditFrag() {
@@ -102,6 +111,7 @@ public class EditFrag extends Fragment {
         View view = inflater.inflate(R.layout.fragment_edit, container, false);
 
         // Declare something needed
+        camera_btn = view.findViewById(R.id.edit_camera);
         avatar_btn = view.findViewById(R.id.edit_avatar_btn);
         avatar = view.findViewById(R.id.edit_avatar);
 
@@ -130,6 +140,15 @@ public class EditFrag extends Fragment {
             }
         });
 
+        // camera upload
+        camera_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePictureIntent, 1001);
+            }
+        });
+
 
         // edit part
         username = view.findViewById(R.id.edit_username);
@@ -144,11 +163,10 @@ public class EditFrag extends Fragment {
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful() && !task.getResult().isEmpty()){
+                        if (task.isSuccessful() && !task.getResult().isEmpty()) {
                             String userString = task.getResult().getDocuments().get(0).get("User Name").toString();
                             username.setText(userString);  // present the current user name on the text-input field
-                        }
-                        else{
+                        } else {
                             Toast.makeText(getActivity(), "Something's Wrong!", Toast.LENGTH_LONG).show();
                         }
                     }
@@ -164,14 +182,17 @@ public class EditFrag extends Fragment {
                 String new_pwd = newpassword.getText().toString().trim();
                 String confirm_pwd = confirmpassword.getText().toString().trim();
 
-                if(user.isEmpty()){
+                // user name cannot be empty
+                if (user.isEmpty()) {
                     username.setError("Username is required!");
                     username.requestFocus();
                     return;
                 }
 
-                if(old_pwd.isEmpty()){
-                    if(new_pwd.length()!=0 || confirm_pwd.length()!=0){
+
+                if (old_pwd.isEmpty()) {
+                    // old password is not entered
+                    if (new_pwd.length() != 0 || confirm_pwd.length() != 0) {
                         oldpassword.setError("Please make sure fill the required fields to change your password");
                         newpassword.setError("Please make sure fill the required fields to change your password");
                         confirmpassword.setError("Please make sure fill the required fields to change your password");
@@ -181,7 +202,7 @@ public class EditFrag extends Fragment {
                     }
 
                     // only change user name
-                    if(new_pwd.isEmpty() && confirm_pwd.isEmpty()){
+                    if (new_pwd.isEmpty() && confirm_pwd.isEmpty()) {
                         Map<String, Object> update = new HashMap<>();
                         update.put("User Name", user);
 
@@ -191,7 +212,7 @@ public class EditFrag extends Fragment {
                                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                                     @Override
                                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                        if (task.isSuccessful() && !task.getResult().isEmpty()){
+                                        if (task.isSuccessful() && !task.getResult().isEmpty()) {
                                             String documentID = task.getResult().getDocuments().get(0).getId();
                                             db.collection("user")
                                                     .document(documentID)
@@ -203,9 +224,7 @@ public class EditFrag extends Fragment {
                                                             getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFrag()).commit();
                                                         }
                                                     });
-                                        }
-
-                                        else{
+                                        } else {
                                             Toast.makeText(getActivity(), "Something Wrong!", Toast.LENGTH_LONG).show();
                                         }
                                     }
@@ -213,30 +232,33 @@ public class EditFrag extends Fragment {
                     }
                 }
 
-                if(!old_pwd.isEmpty()){
+                // tend to change the password
+                if (!old_pwd.isEmpty()) {
                     AuthCredential crediential = EmailAuthProvider.getCredential(fAuth.getCurrentUser().getEmail(), old_pwd);
                     fAuth.getCurrentUser().reauthenticate(crediential)
                             .addOnSuccessListener(new OnSuccessListener<Void>() {
                                 @Override
                                 public void onSuccess(Void unused) {
-                                    if(new_pwd.isEmpty()){
+                                    if (new_pwd.isEmpty()) {
                                         newpassword.setError("Please enter your new password!");
                                         newpassword.requestFocus();
                                     }
 
-                                    if(confirm_pwd.isEmpty()){
+                                    if (confirm_pwd.isEmpty()) {
                                         confirmpassword.setError("Please confirm your new password!");
                                         confirmpassword.requestFocus();
                                     }
 
-                                    if(new_pwd.length() < 7){
+                                    // password should be no less than 7 characters
+                                    if (new_pwd.length() < 7) {
                                         newpassword.setError("Password should be longer than 6 chars");
                                         newpassword.requestFocus();
                                     }
 
-                                    if(!new_pwd.isEmpty() && !confirm_pwd.isEmpty() && new_pwd.length() >= 7){
+                                    // all good
+                                    if (!new_pwd.isEmpty() && !confirm_pwd.isEmpty() && new_pwd.length() >= 7) {
                                         // successfully change password
-                                        if(new_pwd.equals(confirm_pwd)){
+                                        if (new_pwd.equals(confirm_pwd)) {
 
                                             FirebaseUser fUser = fAuth.getInstance().getCurrentUser();
 
@@ -253,7 +275,7 @@ public class EditFrag extends Fragment {
                                                                         .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                                                                             @Override
                                                                             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                                                                if (task.isSuccessful() && !task.getResult().isEmpty()){
+                                                                                if (task.isSuccessful() && !task.getResult().isEmpty()) {
                                                                                     String documentID = task.getResult().getDocuments().get(0).getId();
                                                                                     db.collection("user")
                                                                                             .document(documentID)
@@ -265,9 +287,7 @@ public class EditFrag extends Fragment {
                                                                                                     getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFrag()).commit();
                                                                                                 }
                                                                                             });
-                                                                                }
-
-                                                                                else{
+                                                                                } else {
                                                                                     Toast.makeText(getActivity(), "Something Wrong!", Toast.LENGTH_LONG).show();
                                                                                 }
                                                                             }
@@ -277,19 +297,20 @@ public class EditFrag extends Fragment {
                                                         }
                                                     });
                                         }
-
-                                        else{
+                                        // when confirmed password is not matching with the new password entered
+                                        else {
                                             confirmpassword.setError("The confirmed password doesn't match with the new password!");
                                             confirmpassword.requestFocus();
                                         }
                                     }
                                 }
                             })
+                            //  do not pass the authentication
                             .addOnFailureListener(new OnFailureListener() {
                                 @Override
                                 public void onFailure(@NonNull Exception e) {
-                                   oldpassword.setError("The old password doesn't match with the record!");
-                                   oldpassword.requestFocus();
+                                    oldpassword.setError("The old password doesn't match with the record!");
+                                    oldpassword.requestFocus();
                                 }
                             });
 
@@ -307,43 +328,94 @@ public class EditFrag extends Fragment {
             }
         });
 
-
-
         return view;
     }
+
+
+    // --------added methods-----------
+
+    // this is to check if the camera permission is granted and do further operation
+//    private void checkCamPermission() {
+//        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+//            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.CAMERA}, 101);
+//        }
+//        else {
+//            openCamera();
+//        }
+//    }
+
+
+    // --------added methods-----------
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        if(requestCode == 101){
+//            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+//                startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE), 1001);
+//            }
+//            else{
+//                Toast.makeText(getContext(), "Camera permission needed to proceed", Toast.LENGTH_LONG).show();
+//            }
+//        }
+//    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch(requestCode){
-            case 1000:
-                if(resultCode == Activity.RESULT_OK){
 
-                    Uri avatarUri = data.getData();
+         if(requestCode==1000) {
+             if (resultCode == Activity.RESULT_OK) {
 
-                    // upload to firebase
-                    StorageReference avatarfile = storage.child("users/" + fAuth.getCurrentUser().getUid() + "/avatar.jpg");
-                    avatarfile.putFile(avatarUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            avatarfile.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                @Override
-                                public void onSuccess(Uri uri) {
-                                    // display the new avatar
-                                    Picasso.get().load(uri).into(avatar);
-                                }
-                            });
-                            Toast.makeText(getContext(), "Uploaded Successfully", Toast.LENGTH_LONG).show();
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(getContext(), "Failed", Toast.LENGTH_LONG).show();
-                        }
-                    });
-                }
+                 Uri avatarUri = data.getData();
 
-                break;
-        }
+                 // upload to firebase
+                 StorageReference avatarfile = storage.child("users/" + fAuth.getCurrentUser().getUid() + "/avatar.jpg");
+                 avatarfile.putFile(avatarUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                     @Override
+                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                         avatarfile.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                             @Override
+                             public void onSuccess(Uri uri) {
+                                 // display the new avatar
+                                 Picasso.get().load(uri).into(avatar);
+                             }
+                         });
+                         Toast.makeText(getContext(), "Uploaded Successfully", Toast.LENGTH_LONG).show();
+                     }
+                 }).addOnFailureListener(new OnFailureListener() {
+                     @Override
+                     public void onFailure(@NonNull Exception e) {
+                         Toast.makeText(getContext(), "Failed", Toast.LENGTH_LONG).show();
+                     }
+                 });
+             }
+         }
+
+         if(requestCode==1001) {
+
+             if (resultCode == Activity.RESULT_OK) {
+                 Bitmap ava_bit = (Bitmap) data.getExtras().get("data");
+                 ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                 ava_bit.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+                 byte bb[] = bytes.toByteArray();
+
+                 StorageReference camRef = storage.child("users/" + fAuth.getCurrentUser().getUid() + "/avatar.jpg");
+                 camRef.putBytes(bb).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                     @Override
+                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                         camRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                             @Override
+                             public void onSuccess(Uri uri) {
+                                 // display the new avatar
+                                 Picasso.get().load(uri).into(avatar);
+                             }
+                         });
+                         Toast.makeText(getContext(), "Uploaded Successfully", Toast.LENGTH_LONG).show();
+                     }
+                 });
+             }
+
+         }
     }
 }
