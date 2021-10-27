7fd3334e37f1c042cace7ef1dee74e62a01b9451
Zixin Wang <zixin3@student.unimelb.edu.au>
2021-10-27 17:20:41 +0800

    Firebase callback structure;  update list xml

--- app/src/main/java/com/example/noname/ShopLstFrag.java
+++ app/src/main/java/com/example/noname/ShopLstFrag.java
@@ -1,28 +1,34 @@
 package com.example.noname;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.AbsoluteSizeSpan;
 import android.text.style.RelativeSizeSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -45,11 +51,16 @@ public class ShopLstFrag extends Fragment {
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
+    private CollectionReference itemsRef;
 
     private View view;
     private ListView list;
     private ImageView addItem;
     private String userID;
+
+    // Heart
+    private SimpleAdapter items;
+    private ImageView heart;
 
     public ShopLstFrag() {
         // Required empty public constructor
@@ -72,7 +83,6 @@ public class ShopLstFrag extends Fragment {
     @Override
     public void  onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
         ImageView addButton = getActivity().findViewById(R.id.addItem);
         addButton.setOnClickListener(v -> {
 //            getSupportFragmentManager().beginTransaction()
@@ -95,14 +105,33 @@ public class ShopLstFrag extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_shopping_list, container, false);
         list = view.findViewById(R.id.list);
-        list.setEnabled(false);
 
 
         addItem = view.findViewById(R.id.addItem);
 
+        // get documents from fire store
+        itemsRef = db.collection("list");
+        // read data into listView
+        readData(new FirestoreCallback() {
+            @Override
+            public void onCallback(SimpleAdapter items) {
+                list.setAdapter(items);
+                //list.setClickable(true);
+                Log.d("List", "get adapter after setAdapter: " + list.getAdapter().toString());
+                list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-        db.collection("list")
-                .whereEqualTo("userID", userID)
+                    public void onItemClick(AdapterView adapterView, View view, int position, long id) {
+                        Toast.makeText(getActivity().getApplicationContext(), view.toString(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+        });
+        return view;
+    }
+
+    private void readData(FirestoreCallback firestoreCallback) {
+        itemsRef.whereEqualTo("userID", userID)
                 .orderBy("dueDate")
                 .get()
                 .addOnCompleteListener(task -> {
@@ -128,15 +157,28 @@ public class ShopLstFrag extends Fragment {
                         }
 
                         favoriteItems.addAll(normalItems);
-                        SimpleAdapter items = new SimpleAdapter(getActivity().getApplicationContext(), favoriteItems, R.layout.fragment_shopping_list_item,
+                        items = new SimpleAdapter(getActivity().getApplicationContext(), favoriteItems, R.layout.fragment_shopping_list_item,
                                 new String[]{"favorite", "title", "due"},
-                                new int[]{R.id.favorite, R.id.title, R.id.due});
-                        list.setAdapter(items);
+                                new int[]{R.id.favorite, R.id.title, R.id.due}){
+                            @Override
+                            public View getView(int position, View convertView, ViewGroup parent) {
+                                View view = super.getView(position, convertView, parent);
+                                view.findViewById(R.id.favorite).setTag(position);
+                                view.findViewById(R.id.title).setTag(position);
+                                view.findViewById(R.id.due).setTag(position);
+                                return view;
+                            }
+                        };
+                        firestoreCallback.onCallback(items);
                     } else {
 
                     }
                 });
-
-        return view;
     }
+
+    private interface FirestoreCallback {
+        void onCallback(SimpleAdapter items);
+    }
+
+
 }
