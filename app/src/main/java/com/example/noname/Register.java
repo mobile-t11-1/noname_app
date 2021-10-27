37abdfde64d120ff8d67531680af75dcbe568dc7
xhhh-mendes <kirikirimendes@gmail.com>
2021-10-27 18:07:56 +1000

    Minor

--- app/src/main/java/com/example/noname/Register.java
+++ app/src/main/java/com/example/noname/Register.java
@@ -130,29 +130,6 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
                         if(task.isSuccessful()){
-//                            User user = new User(username, email);
-//
-//                            // pass this user instance to firebase
-//                            // 1. Get a unique UID for this user
-//                            // 2. Check if the data has been successfully passed into database
-//                            FirebaseDatabase.getInstance().getReference("Users")
-//                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-//                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-//                                @Override
-//                                public void onComplete(@NonNull Task<Void> task) {
-//                                    // if the data is in database
-//                                    if(task.isSuccessful()){
-//                                        Toast.makeText(Register.this, "User has been registered successfully!", Toast.LENGTH_LONG).show();
-//                                        progressBar.setVisibility(View.GONE);
-//
-//                                        startActivity(new Intent(Register.this, Login.class));
-//                                    }else{
-//                                        Toast.makeText(Register.this, "Failed to register!1", Toast.LENGTH_LONG).show();
-//                                        progressBar.setVisibility(View.GONE);
-//                                    }
-//
-//                                }
-//                            });
 
                             Map<String, Object> newUser = new HashMap<>();
                             newUser.put("User Name", username);
